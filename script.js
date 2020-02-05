9839266d083f4d53d3b9aa1711eeb108ad81c481
Florin Patrascanu <patrascanu.florin@yahoo.com>
2020-02-05 15:14:02 +0200

    Update

--- script.js
+++ script.js
@@ -36,7 +36,7 @@ var selectUtils = {
         var elementId = '#' + fieldId;
         var selectId = 'Select' + fieldId;
         var parentNode = document.querySelector(elementId).parentNode;
-        parentNode.insertAdjacentHTML('afterend', this.renderStyledSelect(elementId, 'selectUtils.updateSelect(event)', selectId, 'col-lg-6 col-md-6 col-sm-6 col-xs-12 u-no-padding styled-select'));
+        parentNode.insertAdjacentHTML('afterend', this.renderStyledSelect(elementId, 'selectUtils.updateSelect(event)', selectId, 'col-lg-offset-3 col-lg-6 col-md-offset-3 col-md-6 col-sm-12 col-xs-12 u-no-padding styled-select'));
         parentNode.classList.add('u-hidden');
         this.addSearchOptionsToSelect('#' + selectId);
         this.handleOnFocusEnterKeyPress(document.querySelector('#' + selectId));
